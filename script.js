3eb09279e3bc6df1e322bd9c11c1d86dc40cd708
Marcel Ritter <64130092+rittermarcel@users.noreply.github.com>
2021-12-17 19:14:39 +0100

    event listener after video watched

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
+var KlimawandelSchwarzwald;
+(function (KlimawandelSchwarzwald) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        let textBox = document.getElementById("textBox");
+        textBox.hidden = true;
+        let video = document.getElementById("videos");
+        video.onended = () => {
+            videoEnded();
+        };
+        function videoEnded() {
+            console.log("ok");
+            textBox.hidden = false;
+            window.location.href = "#textBox";
+        }
+    }
+})(KlimawandelSchwarzwald || (KlimawandelSchwarzwald = {}));
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,3 +1,18 @@
 namespace KlimawandelSchwarzwald {
-    
+    window.addEventListener("load", handleLoad);
+   
+
+    function handleLoad(): void {
+        let textBox: HTMLDivElement = <HTMLDivElement>document.getElementById("textBox");
+        textBox.hidden = true;
+        let video: HTMLElement = <HTMLElement>document.getElementById("videos");
+        video.onended = () => {
+            videoEnded();
+        };
+        function videoEnded(): void {
+            console.log("ok");
+            textBox.hidden = false;
+            window.location.href = "#textBox";
+        }
+    }
 }
